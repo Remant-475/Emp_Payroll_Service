8413a1277d0a4275fa912067d90ff02e19ef5e8b
Remant <remantmahato9798@gmail.com>
2022-04-21 02:13:25 +0530

    Added : UC2-CreateTableEmployee_Payroll

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -2,3 +2,10 @@
 Create database Payroll_Service;
 select * from sys.databases;
 use Payroll_Service;
+
+--UC2-Create Table Employee_payroll
+Create table Employee_payroll(
+Emp_Id int identity(1,1) not null primary key,
+Emp_Name varchar(50),
+Salary Bigint,
+StartDate Date);
