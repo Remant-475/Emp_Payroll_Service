c5f738164478f216f2f86f84c91ebc4e91ba80bf
Remant <remantmahato9798@gmail.com>
2022-04-21 02:36:29 +0530

    Added : UC3-InsertingValues

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -9,3 +9,11 @@ Emp_Id int identity(1,1) not null primary key,
 Emp_Name varchar(50),
 Salary Bigint,
 StartDate Date);
+
+--UC3- Inserting Values in Table
+INSERT INTO Employee_payroll(Emp_Name,SALARY,StartDate) VALUES 
+('REMANT',25000,'2022-04-18'),
+('AMAN',30000,'2022-03-10'),
+('SHUBHAM',40000,'2022-04-08'),
+('KUMUD',35000,'2022-02-15'),
+('GEETA',45000,'2022-01-06');
